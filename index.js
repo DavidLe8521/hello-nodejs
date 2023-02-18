ae8a463ff627f6442619aa5c4fdbf0fb25d1f449
DavidLe8521 <le.dat1985@gmail.com>
2023-02-18 21:19:02 +0700

    change route dat test

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 app.get('/about', function (req, res) {
-	res.send('Hello world');
+	res.send('Hello world - dat test');
 });
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
-
-
